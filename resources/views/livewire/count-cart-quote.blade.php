e7a84926dc1ecaf227ce21746b91c764cd62a7ab
Oscar-CR <krz.oficial@gmail.com>
2024-06-18 16:37:59 -0600

    cambio de ui de pantalla catalogo

--- resources/views/livewire/count-cart-quote.blade.php
+++ resources/views/livewire/count-cart-quote.blade.php
@@ -1,11 +1,11 @@
 <div>
     <a class="cart-icon nav-link text-black hover:text-primary " aria-current="page" href="{{ route('cotizacion') }}" data-toggle="tooltip"
         data-placement="bottom" title="Cotizacion Actual" >
-        <div class="ml-2 mr-2 mt-4">
+        <div class="ml-2 mr-2" style="margin-top: 10px;">
             @if ($total > 0)
                 <span class="absolute inline-flex items-center justify-center w-4 h-4 text-xs font-bold text-white bg-primary-dark border-1  rounded-full -top-2 -right-2" >{{ $total }}</span>
             @endif
-            <svg width="20px" height="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg width="24px" height="24px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 
                 <defs>
                 <style>.cls-1{fill:none;stroke:#000000;stroke-miterlimit:10;stroke-width:1.91px;}</style>
