97c38d78538f8796582322d919be7eed52a02864
Oscar-CR <krz.oficial@gmail.com>
2024-06-20 16:40:26 -0600

    fix imagen de producto en cotizacion

--- resources/views/pages/catalogo/current-quote-component.blade.php
+++ resources/views/pages/catalogo/current-quote-component.blade.php
@@ -24,16 +24,10 @@
                     <div
                         class="flex justify-between border-t last:border-b border-gray-800 py-3 px-5 gap-2 items-center">
                         <div class="flex items-center">
-                            <div style="width: 2rem">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                    stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                                </svg>
-                            </div>
+                            
                             <div>
                                 <img src="{{ $quote->images_selected ?: ($quote->product->firstImage ? $quote->product->firstImage->image_url : asset('img/default.jpg')) }}"
-                                    alt="" width="100">
+                                    alt="" style="max-width: 200px; height: 100px; object-fit: contain;">
                             </div>
                         </div>
                         <div class="flex-grow space-y-3">
