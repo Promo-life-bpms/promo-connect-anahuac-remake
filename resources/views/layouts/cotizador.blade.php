ad04c0e06abf899dce1738852337a89e115c4875
Oscar-CR <krz.oficial@gmail.com>
2024-06-24 09:14:53 -0600

    agregado de redes sociales

--- resources/views/layouts/cotizador.blade.php
+++ resources/views/layouts/cotizador.blade.php
@@ -44,13 +44,13 @@
 
                     <p class="ml-4 mr-4 text-xl font-bold mt-4">Redes Sociales:</p>
                     <div class="flex ml-4 ">
-                        <a href="">
+                        <a href="https://www.facebook.com/share/JdyjheHUxRn8YySp/?mibextid=LQQJ4d" target="__blank">
                             <svg fill="#FF5900" width="50px" height="50px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M12 2.03998C6.5 2.03998 2 6.52998 2 12.06C2 17.06 5.66 21.21 10.44 21.96V14.96H7.9V12.06H10.44V9.84998C10.44 7.33998 11.93 5.95998 14.22 5.95998C15.31 5.95998 16.45 6.14998 16.45 6.14998V8.61998H15.19C13.95 8.61998 13.56 9.38998 13.56 10.18V12.06H16.34L15.89 14.96H13.56V21.96C15.9164 21.5878 18.0622 20.3855 19.6099 18.57C21.1576 16.7546 22.0054 14.4456 22 12.06C22 6.52998 17.5 2.03998 12 2.03998Z"/>
                             </svg>
                         </a>
                         
-                        <a href="" class="ml-2">
+                        <a href="https://www.instagram.com/redanahuac?igsh=cDQxeG1hMmtjeXV4" class="ml-2" target="__blank">
                             <svg width="50px" height="50px" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                                 <g fill="none" fill-rule="evenodd">
                                     <path d="m0 0h32v32h-32z"/>
@@ -59,7 +59,7 @@
                             </svg>
                         </a>
                        
-                        <a href="" class="ml-3">
+                        <a href="https://x.com/redanahuac?s=11&t=cW5OMwBFHs0GWrKiGBxmpQ" class="ml-3" target="__blank">
                             <svg fill="#FF5900" height="50px" width="50px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
                             viewBox="-143 145 512 512" xml:space="preserve">
                                 <path d="M113,145c-141.4,0-256,114.6-256,256s114.6,256,256,256s256-114.6,256-256S254.4,145,113,145z M215.2,361.2
